705fc712affa7280e67db04b9368e98379c52ad9
AndriyMakarchuk <andriymakarshuk@gmail.com>
2025-01-22 18:54:46 +0200

    Додано таблиці Movies та Genres

---
+++ Cinema/DataAccess/Entities/Genre.cs.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using DataAccess.Interfaces;
+
+namespace DataAccess.Entities
+{
+    public enum GenresEnum
+    {
+        Undefined,
+        Action,
+        Thriller,
+        Mysteries
+    }
+    public class Genre : IEntity
+    {
+        public int Id { get; set; }
+        [Column("GenreName")]
+        public string GenreNameString { get; private set; }
+
+        [NotMapped]
+        private GenresEnum _genreName;
+        [NotMapped]
+        public GenresEnum GenreName { get { return _genreName; } 
+            set 
+            { 
+                GenreNameString = Enum.GetName(typeof(GenresEnum), value);
+                _genreName = value;
+            } 
+        }
+        public List<GenreMovie> Movies { get; set; }
+
+    }
+    public class GenreMovie
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ Cinema/DataAccess/Migrations/20250122165146_MovieTableCreated_5.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovieTableCreated_5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GenreName1",
+                table: "Genres");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GenreName1",
+                table: "Genres",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "GenreName1",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "GenreName1",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "GenreName1",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "GenreName1",
+                value: 3);
+        }
+    }
+}
