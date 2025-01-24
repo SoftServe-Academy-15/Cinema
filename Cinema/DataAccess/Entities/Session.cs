e3116daf61eaaffee990d8e8d19284e40788c8a1
Laybich Inc. <mlaybich@gmail.com>
2025-01-24 21:56:19 +0200

    Implemented Session and Ticket models

---
+++ Cinema/DataAccess/Entities/Session.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Enteties;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess.Entities
+{
+    public class Session : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Movie))]
+        public int MovieId { get; set; }
+
+        [Required]
+        public DateTime SessionDate { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Hall))]
+        public int HallId { get; set; }
+    }
+}
