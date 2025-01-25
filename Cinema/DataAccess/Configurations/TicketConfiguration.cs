880274c457f7e72291636dfddf0204a8b40d6e0d
Laybich Inc. <mlaybich@gmail.com>
2025-01-25 18:28:41 +0200

    Data annotations changed to FluentAPI

---
+++ Cinema/DataAccess/Configurations/TicketConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            // Foreign key
+            builder.HasKey(t => t.Id);
+
+            // Required field
+            builder.Property(t => t.SessionId)
+                .IsRequired();
+
+            // Field SessionId: ForeignKey to Session
+            builder.HasOne<Session>()
+                .WithMany()
+                .HasForeignKey(t => t.SessionId)
+                .OnDelete(DeleteBehavior.Cascade); // Remove all related tickets when Session is deleted
+
+            // Required field
+            builder.Property(t => t.Price)
+                .IsRequired()
+                .HasColumnType("decimal(5,2)");
+
+            // Required field
+            builder.Property(t => t.PlaceRowNumber)
+                .IsRequired();
+
+            // Required field
+            builder.Property(t => t.PlaceColumnNumber)
+                .IsRequired();
+
+            // Required field
+            builder.Property(t => t.UserId)
+                .IsRequired();
+
+            // Field UserId: ForeignKey to User
+            builder.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(s => s.UserId)
+                .OnDelete(DeleteBehavior.Cascade); // Remove all related tickets when User is deleted
+        }
+    }
+}
