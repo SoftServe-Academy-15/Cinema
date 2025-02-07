ba1b0e3591a683863fb325e6bfb7c391e63464df
AndriyMakarchuk <andriymakarshuk@gmail.com>
2025-02-07 17:27:56 +0200

    Додана можливість додавати акторів в базу данних через інтерфейс веб-сторінки.

--- Cinema/BusinessLogic/DTOs/ActorDTO.cs
+++ Cinema/BusinessLogic/DTOs/ActorDTO.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public ushort BirthYear { get; set; }
+        public int BirthYear { get; set; }
         public string PhotoUrl { get; set; }
         public List<RoleDTO> Roles { get; set; }
     }
---
+++ Cinema/WebApp/Controllers/ActorsController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogic.DTOs;
+using BusinessLogic.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IActorService _actorService;
+        public ActorsController(IActorService actorService)
+        {
+            _actorService = actorService;
+        }
+        public IActionResult Index()
+        {
+            return View(_actorService.GetAll());
+        }
+        //GET: /Actors/Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+        //POST: /Actors/Add
+        [HttpPost]
+        public IActionResult Add(ActorDTO actor)
+        {
+            _actorService.Create(actor);
+            return View();
+        }
+    }
+}
